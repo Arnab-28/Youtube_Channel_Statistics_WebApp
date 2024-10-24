780856b804572d27acb516cbacbcbee6fc0f8e93
Arnab Das <70911619+Arnab-28@users.noreply.github.com>
2024-10-24 19:23:07 +0530

    Update Youtube_Channel_Statistics_WebApp.py

--- Youtube_Channel_Statistics_WebApp.py
+++ Youtube_Channel_Statistics_WebApp.py
@@ -1,7 +1,35 @@
 from googleapiclient.discovery import build
 import streamlit as st
+import requests
 
 api_key = 'AIzaSyBvVHqveFW4FLUuk4lh2kn8NMAF7t0QsY4'
+
+def get_channel_id(api_key, channel_name):
+    base_url = "https://www.googleapis.com/youtube/v3/search"
+    
+    # Parameters for the request
+    params = {
+        'part': 'snippet',
+        'q': channel_name,
+        'type': 'channel',
+        'key': api_key
+    }
+    
+    # Making the API request
+    response = requests.get(base_url, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        
+        # If channels are found
+        if 'items' in data and len(data['items']) > 0:
+            # Getting the first channel ID from the search results
+            channel_id = data['items'][0]['snippet']['channelId']
+            return channel_id
+        else:
+            return None
+    else:
+        return None
 
 def get_channel_stats(channel_id):
     
@@ -17,18 +45,18 @@ def get_channel_stats(channel_id):
     Total_Subscribers = response['items'][0]['statistics']['subscriberCount'],
     Total_Views = response['items'][0]['statistics']['viewCount'],
     Total_Videos = response['items'][0]['statistics']['videoCount'],
-    Joinning_Date = response['items'][0]['snippet']['publishedAt'].split('T')[0],
-    Joinning_Time = response['items'][0]['snippet']['publishedAt'].split('T')[1].replace('Z',''))
+    Joinning_Date = response['items'][0]['snippet']['publishedAt'].split('T')[0])
     
     return data
 
 st.title('YouTube Channel Information App')
-st.header('Enter YouTube Channel ID')
+st.header('Enter YouTube Channel Name')
 
-channel_id = st.text_input('Channel ID:', '')
+channel_name = st.text_input('Channel Name:', '')
 
 if st.button('Get Channel Info'):
-    if channel_id:
+    if channel_name:
+        channel_id = get_channel_id(api_key,channel_name)
         channel_info = get_channel_stats(channel_id)
         if channel_info:
             st.subheader('Channel Details')
@@ -37,8 +65,7 @@ if st.button('Get Channel Info'):
             st.write(f"**Total Videos:** {channel_info['Total_Videos']}")
             st.write(f"**Total Views:** {channel_info['Total_Views']}")
             st.write(f"**Channel Joinning Date :** {channel_info['Joinning_Date']}")
-            st.write(f"**Channel Joinning Time :** {channel_info['Joinning_Time']}")
         else:
-            st.error('No data found for the provided Channel ID. Please check the ID and try again.')
+            st.error('No data found for the provided Channel Name. Please check the Name and try again.')
     else:
-        st.warning('Please enter a Channel ID 👆!')
+        st.warning('Please enter a Channel Name 👆')
